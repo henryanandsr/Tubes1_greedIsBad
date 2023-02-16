1a5e144d92bfcf0a5f904b2e74b6f680f6b2bdc7
henryanandsr <13521004@std.stei.itb.ac.id>
2023-02-17 03:01:59 +0700

    update dir

---
+++ src/main/java/Enums/Effect.java
@@ -0,0 +1,9 @@
+// package main.java.Enums;
+
+// public enum Effect{
+//     Afterburner(1),
+//     AsteroidField(2),
+//     GasCloud(4),
+//     Supefood(8),
+//     Shield(16)
+// }
---
+++ src/main/java/Models/GameObject.java
@@ -0,0 +1,90 @@
+package Models;
+
+import Enums.*;
+import java.util.*;
+
+public class GameObject {
+  public UUID id;
+  public Integer size;
+  public Integer speed;
+  public Integer currentHeading;
+  public Position position;
+  public ObjectTypes gameObjectType;
+  public Integer torpedoSalvo;
+  public Integer shield;
+  public Integer afterBurner;
+  public Integer fireTeleport;
+  public Integer teleport;
+  public Integer fireSupernova;
+
+  public GameObject(UUID id, Integer size, Integer speed, Integer currentHeading, Position position, ObjectTypes gameObjectType, Integer torpedoSalvo, Integer Shield, Integer afterBurner, Integer fireTeleport, Integer teleport) {
+    this.id = id;
+    this.size = size;
+    this.speed = speed;
+    this.currentHeading = currentHeading;
+    this.position = position;
+    this.gameObjectType = gameObjectType;
+    this.torpedoSalvo = torpedoSalvo;
+    this.shield = Shield;
+    this.afterBurner = afterBurner;
+    this.fireTeleport = fireTeleport;
+    this.teleport = teleport;
+  }
+
+  public UUID getId() {
+    return id;
+  }
+
+  public void setId(UUID id) {
+    this.id = id;
+  }
+
+  public int getSize() {
+    return size;
+  }
+
+  public void setSize(int size) {
+    this.size = size;
+  }
+  public int getTorpedoSalvo()
+  {
+    return this.torpedoSalvo;
+  }
+  public int getSpeed() {
+    return speed;
+  }
+
+  public void setSpeed(int speed) {
+    this.speed = speed;
+  }
+
+  public Position getPosition() {
+    return position;
+  }
+
+  public void setPosition(Position position) {
+    this.position = position;
+  }
+
+  public ObjectTypes getGameObjectType() {
+    return gameObjectType;
+  }
+
+  public void setGameObjectType(ObjectTypes gameObjectType) {
+    this.gameObjectType = gameObjectType;
+  }
+
+  public static GameObject FromStateList(UUID id, List<Integer> stateList)
+  {
+    Position position = new Position(stateList.get(4), stateList.get(5));
+    // System.out.println(stateList);
+    if (stateList.size() != 11)
+    {
+      return new GameObject(id, stateList.get(0), stateList.get(1), stateList.get(2), position, ObjectTypes.valueOf(stateList.get(3)), null,null,null,null,null);
+    }
+    else
+    {
+      return new GameObject(id, stateList.get(0), stateList.get(1), stateList.get(2), position, ObjectTypes.valueOf(stateList.get(3)), stateList.get(6), stateList.get(7), stateList.get(8),stateList.get(9),stateList.get(10));
+    }
+  }
+}
